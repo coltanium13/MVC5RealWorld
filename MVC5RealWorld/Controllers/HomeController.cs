48f2e0244687bd067fdd31c7208b8178a659d4de
Colton Gardner <cgardner268@gmail.com>
2017-12-19 16:43:48 -0800

    Added Admin Only list of users with partial view

--- MVC5RealWorld/Controllers/HomeController.cs
+++ MVC5RealWorld/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using MVC5RealWorld.Models.ViewModel;
+using MVC5RealWorld.Models.EntityManager;
 
 namespace MVC5RealWorld.Controllers
 {
@@ -29,6 +32,20 @@ namespace MVC5RealWorld.Controllers
 
         public ActionResult UnAuthorized()
         {
+            return View();
+        }
+
+        [AuthorizeRoles("Admin")]
+        public ActionResult ManageUserPartial()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string loginName = User.Identity.Name;
+                UserManager UM = new UserManager();
+                UserDataView UDV = UM.GetUserDataView(loginName);
+                return PartialView(UDV);
+            }
+
             return View();
         }
 
